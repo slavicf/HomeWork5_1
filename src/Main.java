8b6bb7ab7cf88f17ec8d431df410d277ed8ae500
slavicf <jaroslavfrunt@gmail.com>
2017-12-15 16:11:44 +0200

    Домашнее Задание №5(1). generateColor

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main extends Application{
+public class Main extends Application {
     private static final int WINDOW_WIDTH = 480;
-    private static final int WINDOW_HEIGTH = 640;
-    private static final int RGB_PATTERN = 6;
+    private static final int WINDOW_HEIGHT = 640;
+    private static final int RGB_PATTERN = 3;
+    private static final int COLOR_SPAN = 255;
 
     private Random rnd = new Random();
 
@@ -18,40 +20,47 @@ public class Main extends Application{
         launch(args);
     }
 
-    private void windowSetup (Stage primaryStage) {
-        Pane root = new Pane();
-        Scene scene =  new Scene(root);
-
+    private void windowSetup(Stage primaryStage) {
         primaryStage.setTitle("Snowman");
         primaryStage.setResizable(false);
         primaryStage.setWidth(WINDOW_WIDTH);
-        primaryStage.setHeight(WINDOW_HEIGTH);
-        primaryStage.setScene(scene);
+        primaryStage.setHeight(WINDOW_HEIGHT);
     }
 
-    private Paint generateRandomColor() {
+    private Paint generateColor() {
         String rgb = "#";
-        int iColor;
-        char cColor;
 
-        for(int i = 0; i < RGB_PATTERN; i++) {
-            iColor = rnd.nextInt(15);
-
-            rgb += (char)('0' + r.nextInt(9));
+        for (int i = 0; i < RGB_PATTERN; i++) {
+            String temp = Integer.toHexString(rnd.nextInt(COLOR_SPAN));
+            rgb += (temp.length() == 2) ? temp : '0' + temp;
         }
-        System.out.println(rgb);
         return Paint.valueOf(rgb);
     }
 
-    private void draw (int circleNumber, int circleMinRradius, int circleMaxRradius) {
-        int iX, iY, iR, iC;
-        for(int i = 0; i < circleNumber; i++) {
+    private Circle generateCircle(int minRadius, int maxRadius) {
+        Circle c = new Circle(rnd.nextInt(WINDOW_WIDTH),
+                rnd.nextInt(WINDOW_HEIGHT),
+                rnd.nextInt(maxRadius));
 
+        c.setStroke(generateColor());
+        c.setFill(Paint.valueOf("#00000000"));
+
+        return c;
+    }
+
+    private void draw(Pane root, int circlesNumber, int circleMinRradius, int circleMaxRradius) {
+        int iX, iY, iR, iC;
+        for (int i = 0; i < circlesNumber; i++) {
+            Circle circle = generateCircle(circleMinRradius, circleMaxRradius);
+            root.getChildren().addAll(circle);
         }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
+
         int circleNumber, circleMinRradius, circleMaxRradius;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите количество кругов: ");
@@ -62,7 +71,8 @@ public class Main extends Application{
         circleMaxRradius = scanner.nextInt();
 
         windowSetup(primaryStage);
-
+        draw(root, circleNumber, circleMinRradius, circleMaxRradius);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }

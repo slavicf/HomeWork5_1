8b7c0ccb00dc9b55ea52da9c9a938d802d88e8a6
slavicf <jaroslavfrunt@gmail.com>
2017-12-18 12:56:15 +0200

    Домашнее Задание №5(1).Задание 2. Финал

--- src/star/Main.java
+++ src/star/Main.java
@@ -9,69 +9,78 @@ import javafx.stage.Stage;
 import java.util.Scanner;
 
 public class Main extends Application {
-    //  --------------------------- declarations ---------------------------------------
-    private static final double WINDOW_WIDTH = 400;             // Ширина окна
-    private static final double WINDOW_HEIGHT = 400;            // Высота окна
-    private static final int VERTEX_NUM = 5 * 2;                // Количество вершин звезды (внешних и внутренних)
-    private static final double ANGLE = 360 / VERTEX_NUM;       // Угол поворота
-    private static final double RATIO = 2.6180339887498955;     // Соотношение радиусов внешних углов к внутренним
 
-    private static Point center = new Point();                  // Центр звезды
-    private static double radius = 200;                         // Радиус звезды
-    private static double degree = -90;                         // Начальный угол
-    private static Point[] vertex = new Point[VERTEX_NUM];      // Массив вершин
+    private static final double WINDOW_WIDTH = 400;         // Ширина окна
+    private static final double WINDOW_HEIGHT = 400;        // Высота окна
+    private static final int VERTEX_NUM = 5 * 2;            // Количество вершин звезды (внешних и внутренних)
+    private static final double ANGLE = 360 / VERTEX_NUM;   // Угол поворота
+    private static final double RATIO = 2.618033988749895;  // Оотношение радиусов внешних вершин звезды к внутренним (квадрат золотого сечения)
+
+    private static Point center = new Point();              // Центр звезды
+    private static double radius = WINDOW_WIDTH / 2;        // Радиус звезды
+    private static double degree = -90;                     // Начальный угол
+    private static Point[] vertex = new Point[VERTEX_NUM];  // Массив вершин
+    // Объявление констант, переменных, структур
 
     public static void main(String[] args) {
         launch(args);
-    }
+    }   // Запуск приложения
 
     void windowSetup(Stage primaryStage) {
         primaryStage.setTitle("Star");
         primaryStage.setResizable(false);
         primaryStage.setWidth(WINDOW_WIDTH);
         primaryStage.setHeight(WINDOW_HEIGHT);
-    }
+    }   // Настройка окна
 
-    private void draw(Pane root) {
-        int i, j;
-        double startX, startY, endX, endY;
+    private void setVertex(int i) {
+        double x, y;
+        double ratio = (i % 2 == 0) ? 1 : RATIO;    // Для внешних вершин отношение = 1, для внутренних = RATIO
+        x = radius / ratio * Math.cos(Math.toRadians(degree));
+        y = radius / ratio * Math.sin(Math.toRadians(degree));
+        vertex[i] = new Point(x, y);
+        degree += ANGLE;
+    }   // Установка вершины звезды
 
-        for (i = 0; i < VERTEX_NUM; i++) {
-            vertex[i] = new Point(radius * Math.cos(Math.toRadians(degree)),
-                    radius * Math.sin(Math.toRadians(degree)));
-            degree += ANGLE;
-            vertex[++i] = new Point(radius / RATIO * Math.cos(Math.toRadians(degree)),
-                    radius / RATIO * Math.sin(Math.toRadians(degree)));
-            degree += ANGLE;
+    private void fillVertex() {
+        for (int i = 0; i < VERTEX_NUM; i++) {
+            setVertex(i++);  // Установка внешней вершины звезды
+            setVertex(i);   // Установка внутренней вершины звезды
         }
+    }   // Заполнение массива вершин звезды
 
-        for (i = 0; i < VERTEX_NUM; i++) {
-            j = (i < VERTEX_NUM - 1) ? i + 1 : 0;
-            startX = vertex[i].x + center.x;
-            startY = vertex[i].y + center.y;
-            endX = vertex[j].x + center.x;
-            endY = vertex[j].y + center.y;
-            Line line = new Line(startX, startY,endX, endY);
-            root.getChildren().addAll(line);              // Строим
+    private void drawStar(Pane root) {
+        fillVertex();    // Заполнение массива вершин звезды
+
+        for (int i = 0; i < VERTEX_NUM; i++) {
+            int j = (i < VERTEX_NUM - 1) ? i + 1 : 0;   // Указатель на конечную точку
+            Line line = new Line();
+            line.setStartX(vertex[i].x + center.x);
+            line.setStartY(vertex[i].y + center.y);
+            line.setEndX(vertex[j].x + center.x);
+            line.setEndY(vertex[j].y + center.y);
+            root.getChildren().addAll(line);
         }
+    }   // Отрисовка звезды
 
-    }
-
-        @Override
+    @Override
     public void start(Stage primaryStage) throws Exception {
-            Pane root = new Pane();
-            Scene scene = new Scene(root);
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Введите координату X центра звезды: ");     center.x = scanner.nextInt();
-            System.out.print("Введите координату Y центра звезды: ");     center.y = scanner.nextInt();
-            System.out.print("Введите радиус звезды: ");                  radius = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите координату X центра звезды: ");
+        center.x = scanner.nextInt();
+        System.out.print("Введите координату Y центра звезды: ");
+        center.y = scanner.nextInt();
+        System.out.print("Введите радиус звезды: ");
+        radius = scanner.nextInt();
 
-            windowSetup(primaryStage);                      // Инициализация окна
+        windowSetup(primaryStage);                      // Инициализация окна
 
-            draw(root);                                     // Построение
+        drawStar(root);                                     // Построение
 
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
